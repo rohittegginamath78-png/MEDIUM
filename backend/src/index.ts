deab336c652bba150aeb5d9dd93eea1432e7ee70
Rohit Tegginamath <rohittegginamath78@gmail.com>
2026-03-07 20:04:15 +0000

    fix(backend) : added cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,11 +2,11 @@ import { Hono } from "hono";
 import { userRouter } from "./Routes/user";
 import { Bindings } from "./types";
 import { blogRouter } from "./Routes/blogs";
-
+import { cors } from "hono/cors";
 const app = new Hono<{
   Bindings: Bindings
 }>();
-
+app.use("*" , cors());
 app.route("/api/v1", userRouter);
 
 app.route("/api/v1" ,blogRouter)
